0b7e9935d2b6003e81b3df44a3bb9998f1fd0f29
7Flafleur <7nb@hotmail.fr>
2024-01-09 20:40:43 +0100

    get photographers

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,54 +1,38 @@
 
-    
-
- 
-   
-
-
-
-
-
- 
-
-//    console.log("Result getphotographers function", getPhotographers())
-   
-   
-   
-   
-   
-   async function getPhotographers() {
-    let req=fetch('/data/photographers.json');
-    req.then(response => {    
-         return response.json();  
-     }
-     )
-     .then(result=>{
-        console.log("photographers:", result.photographers)
-         return result.photographers;
-     })
-     
-    }
-
-
-
-
-
-
-//     async function displayData(photographers) {
-//         const photographersSection = document.querySelector(".photographer_section");
-
-//         photographers.forEach((photographer) => {
-//             const photographerModel = photographerTemplate(photographer);
-//             const userCardDOM = photographerModel.getUserCardDOM();
-//             photographersSection.appendChild(userCardDOM);
-//         });
-//     }
-
-//     async function init() {
-//         // Récupère les datas des photographes
-//         const { photographers } = await getPhotographers();
-//         displayData(photographers);
-//     }
-    
-//     init();
-    
+//get the .json data from server, return promise
+async function fetchData() {
+    let response = await fetch('/data/photographers.json');
+    let data = await response.json();
+    let photographers = data.photographers;
+    return photographers;
+  }
+  
+  //await promise, return array
+  async function getPhotographers() {
+    let photographers = await fetchData();
+    return photographers;
+  }
+  
+  // take array, target html section
+  async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+  
+    //use template for each photographer in array, pass each photographer to function
+    photographers.forEach((photographer) => {
+      const photographerModel = photographerTemplate(photographer);
+      const userCardDOM = photographerModel.getUserCardDOM();
+      photographersSection.appendChild(userCardDOM);
+    });
+  }
+  
+  async function init() {
+    // Récupère les datas des photographes
+    const photographers = await getPhotographers();
+    displayData(photographers);
+  }
+  
+  // Call the init function within an async context
+  (async () => {
+    await init();
+  })();
+  
