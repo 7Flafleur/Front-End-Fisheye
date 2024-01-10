541d4a6d99356496e1bef007669c3e6906267c10
7Flafleur <7nb@hotmail.fr>
2024-01-10 12:00:25 +0100

    extract data json

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -2,6 +2,13 @@ function photographerTemplate(data) {
     const { name, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
+        const id  = `assets/photographers/${id}`;
+        const city = `assets/photographers/${city}`;
+        const country = `assets/photographers/${country}`;
+        const tagline = `assets/photographers/${tagline}`;
+        const price = `assets/photographers/${price}`;
+
+
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
@@ -9,21 +16,23 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        const  = document.createElement
-        const  = document.createElement
-        const  = document.createElement
-        const  = document.createElement
-        const  = document.createElement
+
         article.appendChild(img);
         article.appendChild(h2);
-        article.appendChild();
-        article.appendChild();
-        article.appendChild();
-        article.appendChild();
-        article.appendChild();
+        
+        // article.appendChild();
+        // article.appendChild();
+        // article.appendChild();
+        // article.appendChild();
 
 
         return (article);
     }
+
+
+
     return { name, picture, getUserCardDOM }
+
+
+
 }
