a8dc4e381f7ea4ff58147efc6786d49844dfbaf1
7Flafleur <7nb@hotmail.fr>
2024-01-10 12:40:33 +0100

    return

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -2,24 +2,30 @@ function photographerTemplate(data) {
     const { name,id,city,country,tagline,price, portrait, } = data;
 
         const picture = `assets/photographers/${portrait}`;
-        const idhtml  = `assets/photographers/${id}`;
-        const cityh = `assets/photographers/${city}`;
-        const countryh = `assets/photographers/${country}`;
-        const taglineh = `assets/photographers/${tagline}`;
-        const priceh = `assets/photographers/${price}`;
+        const idhtml  = `${id}`;
+        const cityh = `${city}`;
+        const countryh = `${country}`;
+        const taglineh = `${tagline}`;
+        const priceh = `${price}`;
 
-        console.log(idhtml)
-        console.log
-
+        // console.log(idhtml)
+        // console.log(cityh)
+        // console.log(countryh)
+        // console.log(taglineh)
+        // console.log(priceh)
+       
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        article.setAttribute("id",idhtml)  // create id for each article
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
 
-        article.setAttribute("id",idhtml)
+
+        
 
 
 
@@ -38,7 +44,7 @@ function photographerTemplate(data) {
 
 
 
-    return { name, picture,idhtml, getUserCardDOM }
+    return { name, picture,idhtml,cityh,countryh,taglineh,priceh, getUserCardDOM }
 
 
 
