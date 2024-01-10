019b748d77e69f3ff9ea2db25fe4969cafcfc97e
7Flafleur <7nb@hotmail.fr>
2024-01-10 12:59:27 +0100

    etape2ok?

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -24,6 +24,16 @@ function photographerTemplate(data) {
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
 
+        const h3 = document.createElement('h3');
+        let text = cityh + "," + countryh
+        h3.textContent = text;
+
+        const divtag = document.createElement('div');
+        divtag.textContent=taglineh;
+
+        const pricetag = document.createElement('div');
+        pricetag.textContent = priceh +"€/jour";
+
 
         
 
@@ -32,6 +42,9 @@ function photographerTemplate(data) {
 
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(divtag);
+        article.appendChild(pricetag);
         
         // article.appendChild();
         // article.appendChild();
