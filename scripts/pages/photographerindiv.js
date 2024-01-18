85fb374f0a4ecca9cc42b0255b218aeb98be4c7a
7Flafleur <7nb@hotmail.fr>
2024-01-18 11:28:28 +0100

    tests

--- scripts/pages/photographerindiv.js
+++ scripts/pages/photographerindiv.js
@@ -2,6 +2,9 @@
 const currentURLsearch =new URLSearchParams(window.location.search);
 const urlid=(currentURLsearch.get("id"));
 
+
+
+
 console.log("URLId",urlid)
 
 
@@ -9,39 +12,56 @@ console.log("URLId",urlid)
 
 //use function from index.js script
 
-async function fetchData() {
-  let response = await fetch('./data/photographers.json');
-  let data = await response.json();
-  let photographers = data.photographers;
-  return photographers;
-}
+// async function fetchPhotoData() {
+//   let response = await fetch('./data/photographers.json');
+//   let data = await response.json();
+//   let photographers = data.photographers;
+//   console.log(photographers);
+//   return photographers;
+// }
 
-//get data for header
+// //get data for header,await promise, return object
 
-async function getPhotoHeader(){
-let data = await fetchData();
-console.log("Data",data)
+// async function getPhotoHeader(){
+// let photographers = await fetchPhotoData();
+//  console.log("Data",photographers)
 
-data.forEach((object)=>{
+// ///object for photographer
+
+
+// //get JSON object for photographer from array of objects
+// photographers.forEach((object)=>{
+//   if(object.photographerId==urlid){
+//      PhotographerData = object;
+//      console.log("Match found!")
+//   }
+// })
+
+// return PhotographerData;
+// }
+
+// if (!PhotographerData) {
+//   console.log("No matching photographer found.");
+//   // Handle non-matching case, e.g., redirect, display an error, etc.
+// }
+
+
+// async function displayHeader(PhotographerData) {
+//   const headerSection = document.querySelector(".photograph-header")
+
+//   //model from media function
   
-  let PhotographerData={}
-  if(object.photographerId==urlid){
-     PhotographerData = object;
-  }  
-})
-return PhotographerData;
-}
+//     const headerModel = photographerTemplate(PhotographerData);  //returns video or image mediamodel
+//     const headerCardDOM = headerModel.getUserHeaderDOM();  // function creates mediacard that has either video or img tag, according to object type
+//     headerSection.appendChild(headerCardDOM);
+//   ;
+// }
 
-async function displayHeader(indivmedia) {
-  const headerSection = document.querySelector(".phtotograph-header")
 
-  //use template for each media in array, pass each media to function  according to its type
-  ((item) => {
-    const headerModel = MediaTemplate(item);  //returns video or image mediamodel
-    const mediaCardDOM = mediaModel.getUserHeaderDOM();  // function creates mediacard that has either video or img tag, according to object type
-    mediaSection.appendChild(mediaCardDOM);
-  });
-}
+
+
+
+////////////////
 
 
 //get media
@@ -49,16 +69,18 @@ async function fetchMediaData() {
   let response = await fetch('./data/photographers.json');
   let data = await response.json();
   let media = data.media;
-  return media;
+  return media;        ////////////array of JSON objects
 }
 
 //await promise, return array
 async function getMedia() {
-  let media = await fetchMediaData();
-  console.log("media:",media)
+  let media = await fetchMediaData();         ///
+  // console.log("media:",media)
   
-  //get array for individual photographer
+  //array for photographer
   let indivmedia = new Array();
+
+  //fill empty array for individual photographer
   media.forEach((object)=>{ 
     
     if(object.photographerId==urlid){
@@ -89,6 +111,8 @@ async function displayMedia(indivmedia) {
 async function init() {
   // Get data from JSON file
   const indivmedia = await getMedia();
+  // const headermedia = await getPhotoHeader();
+  // displayHeader(headermedia);
   displayMedia(indivmedia);
 }
 
