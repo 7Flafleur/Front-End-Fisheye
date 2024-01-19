5d630661f92f6ffcd0c01f5aed986bba54daabd0
7Flafleur <7nb@hotmail.fr>
2024-01-19 13:26:30 +0100

    async tests

--- scripts/pages/test.js
+++ scripts/pages/test.js
@@ -1,48 +1,51 @@
+//get the .json data from server, return promise
+async function fetchData() {
+    let response = await fetch('./data/photographers.json');
+    let data = await response.json();
+    let photographers = data.photographers;
+    return photographers;
+  }
 
-//get photographer Id from URL
-// const currentURLsearch =new URLSearchParams(window.location.search);
-// const urlid=(currentURLsearch.get("id"));
-
-
-
-// console.log(dataargument);
-
-// dataargument.forEach((person)=>{
-//     if(person.id==urlid){
-//         console.log("Match found!")
-//         console.log(person)
-//         return person;
-//     }
-// })
-
-
-// function findperson(dataparameter,criteria){
-//     for(person in dataparameter){
-//         if(person.id==criteria){
-//             console.log("Match found!")
-//             return person;
-//         }
-//         else{
-//             console.log("No match found!")
-//         }   
-//     }
-    
+    //await promise, return array
+    async function getPhotographers() {
+        let photographers = await fetchData();
+        console.log(photographers)
+        return photographers;
+      }
 
 
 //////////Main code//////////
 
- let Theperson = findperson(dataargument, urlid);
+async function init() {
+    const photographers = await getPhotographers();
+    const person = findperson(photographers, urlid);
+    displayHeader(person);
+    
+}
 
-console.log("Found Person:", Theperson);
-console.log("Person is :", typeof (Theperson));
-
-let deconstructedPerson = HeaderTemplate(Theperson);
-
-console.log("Deconstructed person:", deconstructedPerson)
-console.log("Deconstructed person is an ", typeof (deconstructedPerson))
+  // Call the init function within an async context
+  (async () => {
+    await init();
+  })();
+  
 
 
-displayHeader(Theperson);
+
+
+
+
+//  let Theperson = findperson(dataargument, urlid);
+
+// console.log("Found Person:", Theperson);
+// console.log("Person is :", typeof (Theperson));
+
+// let deconstructedPerson = HeaderTemplate(Theperson);
+
+// console.log("Deconstructed person:", deconstructedPerson)
+// console.log("Deconstructed person is an ", typeof (deconstructedPerson))
+
+
+// displayHeader(Theperson);
 
 
 
