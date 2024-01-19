ad1e6964faf385a86aa25be73b2b1228683b1034
7Flafleur <7nb@hotmail.fr>
2024-01-19 09:31:18 +0100

    pricetag

--- scripts/templates/header.js
+++ scripts/templates/header.js
@@ -2,7 +2,7 @@ const urlid = 243;
 
 
 
-let dataargument=[
+let dataargument = [
     {
         "name": "Mimi Keel",
         "id": 243,
@@ -75,73 +75,73 @@ function findperson(dataparameter, criteria) {
 
 
 function HeaderTemplate(dataparameter) {
-    const { name,id,city,country,tagline,price, portrait, } = dataparameter;
+    const { name, id, city, country, tagline, price, portrait, } = dataparameter;
 
-        const picture = `./assets/photographers/${portrait}`;
-        const idhtml  = `${id}`;
-        const cityh = `${city}`;
-        const countryh = `${country}`;
-        const taglineh = `${tagline}`;
-        const priceh = `${price}`;
+    const picture = `./assets/photographers/${portrait}`;
+    const idhtml = `${id}`;
+    const cityh = `${city}`;
+    const countryh = `${country}`;
+    const taglineh = `${tagline}`;
+    const priceh = `${price}`;
 
-        function getuserHeaderDOM(){
+    function getuserHeaderDOM() {
 
-            //create section container for DOM elements
-            const userHeaderDOM=document.createElement('section');
-            userHeaderDOM.setAttribute("class", "headercontainer");
-        
-            //infocontainer
-            const infos =document.createElement('div');
-            infos.setAttribute("class","infos")
-        
-            const h2 = document.createElement( 'h1' );
-            h2.textContent = name;
-        
-            const h3 = document.createElement('h2');
-            let text = cityh + "," + countryh
-            h3.innerHTML = text;
-        
-            const blockquote = document.createElement('blockquote');
-            blockquote.textContent=taglineh;
-        
-            infos.append(h2,h3,blockquote);
-        
-            //button
-        
-            const button=document.createElement("button")
-            button.setAttribute("class","contact_button")
-            button.setAttribute("onclick","displayModal()")
-            button.innerHTML="Contactez-moi";
-        
-            //img
-            
-            const figure=document.createElement("figure");
-            figure.setAttribute("class","portrait" )
-            const img = document.createElement( 'img' );
-            img.setAttribute("src", picture)
-            let imgatt= "image de " + name
-            img.setAttribute("alt",imgatt)
-        
-            figure.appendChild(img)
-        
-            userHeaderDOM.append(infos,button,img)
-        
-        
-            
-            
-            //return container
-               return userHeaderDOM
-        
-         }
+        //create section container for DOM elements
+        const userHeaderDOM = document.createElement('section');
+        userHeaderDOM.setAttribute("class", "headercontainer");
 
-    return { name, picture,idhtml,cityh,countryh,taglineh,priceh,getuserHeaderDOM }
+        //infocontainer
+        const infos = document.createElement('div');
+        infos.setAttribute("class", "infos")
+
+        const h2 = document.createElement('h1');
+        h2.textContent = name;
+
+        const h3 = document.createElement('h2');
+        let text = cityh + "," + countryh
+        h3.innerHTML = text;
+
+        const blockquote = document.createElement('blockquote');
+        blockquote.textContent = taglineh;
+
+        infos.append(h2, h3, blockquote);
+
+        //button
+
+        const button = document.createElement("button")
+        button.setAttribute("class", "contact_button")
+        button.setAttribute("onclick", "displayModal()")
+        button.innerHTML = "Contactez-moi";
+
+        //img
+
+        const figure = document.createElement("figure");
+        figure.setAttribute("class", "portrait")
+        const img = document.createElement('img');
+        img.setAttribute("src", picture)
+        let imgatt = "image de " + name
+        img.setAttribute("alt", imgatt)
+
+        figure.appendChild(img)
+
+        userHeaderDOM.append(infos, button, img)
+
+
+
+
+        //return container
+        return userHeaderDOM
+
+    }
+
+    return { name, picture, idhtml, cityh, countryh, taglineh, priceh, getuserHeaderDOM }
 
 }
 
-function displayHeader(dataobject){
-    const headersection=document.querySelector(".photograph-header")
-    const personmodel=HeaderTemplate(dataobject)
-    const headercard=personmodel.getuserHeaderDOM();
+function displayHeader(dataobject) {
+    const headersection = document.querySelector(".photograph-header")
+    const personmodel = HeaderTemplate(dataobject)
+    const headercard = personmodel.getuserHeaderDOM();
     headersection.append(headercard);
 
 }
@@ -150,17 +150,17 @@ function displayHeader(dataobject){
 
 
 
- //////////Main code//////////
+//////////Main code//////////
 
-let Theperson = findperson(dataargument,urlid);
+let Theperson = findperson(dataargument, urlid);
 
 console.log("Found Person:", Theperson);
-console.log("Person is :",typeof(Theperson));
+console.log("Person is :", typeof (Theperson));
 
 let deconstructedPerson = HeaderTemplate(Theperson);
 
-console.log("Deconstructed person:",deconstructedPerson)
-console.log("Deconstructed person is an ",typeof(deconstructedPerson))
+console.log("Deconstructed person:", deconstructedPerson)
+console.log("Deconstructed person is an ", typeof (deconstructedPerson))
 
 
 displayHeader(Theperson);
