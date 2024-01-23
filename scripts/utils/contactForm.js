826849694e69f3ebdab1da83f27679cb7c4f65bc
7Flafleur <7nb@hotmail.fr>
2024-01-23 12:47:49 +0100

    bye

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,15 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+    const form = document.getElementById("hi");
+    // form.style.display = "flex"; TEST
+
+    const bye = document.getElementById("bye");
+
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
