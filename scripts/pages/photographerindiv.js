b454f4dba1ed7634e326a9f1e3c896582db172a4
7Flafleur <7nb@hotmail.fr>
2024-01-24 11:23:59 +0100

    lightbox

--- scripts/pages/photographerindiv.js
+++ scripts/pages/photographerindiv.js
@@ -83,7 +83,72 @@ async function init() {
   displayMedia(indivmedia);
   insertNameForm(person);
   addPriceTag(person);
+  createLightbox();
 }
+
+//create lightbox element
+
+function createLightbox() {
+
+  const lightbox=document.createElement("div");
+lightbox.id="lightbox";
+// set aria role 
+lightbox.setAttribute("role","dialog");
+
+document.body.appendChild(lightbox);
+
+// loop through all images in mediasection, add event listener to each
+const pictures=document.querySelectorAll(".media_section img");
+pictures.forEach((picture)=>{
+  picture.addEventListener("click",e=>{
+    //on image click, add active class to lightbox
+    lightbox.classList.add("active");
+    //create img element, set src to clicked image
+    const img=document.createElement("img");
+    img.src=picture.src;
+    //if lightbox has child, remove it,so no double images
+    while(lightbox.firstChild){
+      lightbox.removeChild(lightbox.firstChild);
+    }
+    //append img to display to lightbox
+    lightbox.appendChild(img);
+  })
+})
+
+
+}
+
+
+// const lightbox=document.createElement("div");
+// lightbox.id="lightbox";
+// // set aria role 
+// lightbox.setAttribute("role","dialog");
+
+// document.body.appendChild(lightbox);
+
+// // loop through all images in mediasection, add event listener to each
+// const pictures=document.querySelectorAll(".media_section img");
+// pictures.forEach((picture)=>{
+//   picture.addEventListener("click",e=>{
+//     //on image click, add active class to lightbox
+//     lightbox.classList.add("active");
+//     //create img element, set src to clicked image
+//     const img=document.createElement("img");
+//     img.src=picture.src;
+//     //if lightbox has child, remove it,so no double images
+//     while(lightbox.firstChild){
+//       lightbox.removeChild(lightbox.firstChild);
+//     }
+//     //append img to display to lightbox
+//     lightbox.appendChild(img);
+//   })
+// })
+
+
+
+
+
+
 
 // MAIN CODE Call the init function within an async context 
 (async () => {
