5e29042b7c3f66b5a41fc829c81bf49997b95469
7Flafleur <7nb@hotmail.fr>
2024-01-25 10:29:52 +0100

    sliders

--- scripts/utils/carousel.js
+++ scripts/utils/carousel.js
@@ -1,3 +1,27 @@
-function createCArousel(){
+const mediaLinks=Array.from(document.querySelectorAll(".mediacard"));
+console.log("medialinks: ",mediaLinks);
+
+
+mediaLinks.forEach((link)=>{
+    addEventListener("click",()=>{
+        console.log("clicked");
+    })
     
+});
+
+mediaLinks.forEach((link)=>{
+    console.log("mediacard:",link);
+    console.log("source:",link.dataset.source);
+    createCarousel(mediaLinks)
+});
+
+
+function createCarousel(mediaLinks){
+
+    mediaLinks.forEach((link)=>{
+        const li= document.createElement('li')
+        li.classList.add("slide")
+    })
+    
+
 }
